b59ef1ecdaeafefb5300de1affff4e3528e4d8ed
Hazrodd <hleray@nus.edu.sg>
2026-04-09 22:01:48 +0800

    Fix photo filename case

--- header.js
+++ header.js
@@ -1,6 +1,6 @@
 document.getElementById('site-header').innerHTML = `
   <header>
-    <img src="HugoPhoto.JPEG" alt="Hugo Leray Photo">
+    <img src="HugoPhoto.jpeg" alt="Hugo Leray Photo">
     <h1>Hugo Leray</h1>
     <p>Space Systems Engineer</p>
   </header>
